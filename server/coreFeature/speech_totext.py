e91114d8c0c8a8a23f8866e673ef368c78479c74
chai41104 <chai41104@hotmail.com>
2016-11-06 08:36:09 +0000

    add

--- server/coreFeature/speech_totext.py
+++ server/coreFeature/speech_totext.py
@@ -5,8 +5,8 @@ from watson_developer_cloud import speech_to_text_v1
 
 def get_me_text(audioFile):
     speech = speech_to_text_v1(
-        username='fb3a4eef-3535-4ea1-9920-5f944f53spee6501',
-        password='C4lmPJgiupQB',)
+        username='18ab33e2-0567-4d02-ad63-266171c3b14d',
+        password='rPBiaHH8L4Ji',)
     try:
         return json.dumps(speech.recognize(audio=audioFile))
     except:
