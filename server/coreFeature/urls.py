3d234b2c1a12c1b2eb39affdde26670810345058
chai41104 <chai41104@hotmail.com>
2016-11-06 01:20:04 +0000

    push api

--- server/coreFeature/urls.py
+++ server/coreFeature/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url
-
-from . import views
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+import views
 
 urlpatterns = [
     # url(r'^(?P<api>[a-z | A-Z]+)', views.index, name='index'),
-    url('^$', views.index, name='index'),
+    url(r'^ToneAnalize/', views.ToneAnalize, name='ToneAnalize'),
+    url(r'^PersonalityInsights/', views.PersonalityInsights, name='PersonalityInsights'),
+	url(r'^AlchemyLanguage/', views.AlchemyLanguage, name='AlchemyLanguage'),
 ]
